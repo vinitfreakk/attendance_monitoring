00616cca702f5e1ddf3e6e1e4dda2811a6e4b3f1
vinit <vinithanabar20@gmail.com>
2023-05-23 09:25:38 +0530

    added only the login and registration activity

---
+++ app/src/main/java/com/example/attendancemontering/RegistartionActivity.java
@@ -0,0 +1,14 @@
+package com.example.attendancemontering;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistartionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registartion);
+    }
+}
